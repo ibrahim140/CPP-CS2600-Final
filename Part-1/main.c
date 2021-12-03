9c6834fe39fa5136ab1132b120afe688d16b8507
Mohammed Ibrahim <mohammedi@cpp.edu>
2021-12-03 14:28:44 -0800

    add lsh_read_line function and its simplified version as multiline comment

--- Part-1/main.c
+++ Part-1/main.c
@@ -26,6 +26,69 @@ void lsh_loop()
     } while(status); // exit condition
 }
 
+char *lsh_read_line(void)
+{
+    //variable declarations
+    int buffsize = LSH_RL_BUFFSIZE, position = 0, c;
+    char *buffer = malloc(sizeof(char) *buffsize);
+
+    // check for buffer allocation and print error if necessary
+    if(!buffer)
+    {
+        fprintf(stderr, "LSH: Allocation Error.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1)
+    {
+        c = getchar();
+        // check 'c' for end of file or newline 
+        if(c == EOF || c == '\n');
+        {
+            buffer[position] = NULL; // null terminate current string
+            return buffer; // return current string
+        }
+        else
+        {
+            buffer[position] = c; // add c to current string
+        }
+        position++; // increment position of c in line
+
+        // check if buffer size is enough
+        if(position >= buffsize) 
+        {
+            // reallocate buffer
+            buffsize += LSH_RL_BUFFSIZE;
+            buffer = realloc(buffer, buffsize);
+            // check for buffer allocation and print error if necessary
+            if(!buffer)
+            {
+                fprintf(stderr, "LSH: Allocation Error.\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    /*
+    simpifying lsh_read_line function with "getline" function
+    char *line = NULL;
+    ssize_t buffsize = 0;
+    if(getline(&line, &buffsize, stdin) == -1)
+    {
+        if(feof(stdin))
+        {
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            perror("Read Line");
+            exit(EXIT_FAILURE);
+        }
+    }
+    return line;
+    */
+}
+
 int main(int argc, char **argv)
 {
     // load config files & run command loop.
