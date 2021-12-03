fa3d240ea4db695b6527ac6ad296bc7e07033aef
Mohammed Ibrahim <mohammedi@cpp.edu>
2021-12-03 13:39:30 -0800

    add lsh_loop function

--- Part-1/main.c
+++ Part-1/main.c
@@ -8,6 +8,24 @@
 #include <stdio.h>
 #include <string.h>
 
+
+void lsh_loop()
+{
+    char *line, **args;
+    int status;
+
+    do
+    {
+        printf("> "); // print prompt
+        line = lsh_read_line(); // function to read line
+        args = lsh_split_line(line); // split the previous line into arguments
+        status = lsh_execute(args); // execute the arguments from the line
+
+        free(line); // free line for next line
+        free(args); // free args for next line arguments
+    } while(status); // exit condition
+}
+
 int main(int argc, char **argv)
 {
     // load config files & run command loop.
