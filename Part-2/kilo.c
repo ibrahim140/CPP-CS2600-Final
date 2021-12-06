d9cb3246cfd1af3d4392dca5f2eb025b9d27733b
Mohammed Ibrahim <mohammedi@cpp.edu>
2021-12-06 15:54:05 -0800

    update method(s) for getting window size

--- Part-2/kilo.c
+++ Part-2/kilo.c
@@ -74,12 +74,53 @@ void editorReadKey()
     return c;
 }
 
+int getCursorPosition(int *rows, int *cols)
+{
+    char buff[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+    {
+        return -1;
+    }
+    
+    while (i < sizeof(buf) - 1)
+    {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1)
+        {
+            break;
+        }
+        if (buff[i] == 'R')
+        {
+            break;
+        }
+        i++;
+    }
+    buff[i] = '\0';
+    
+    if (buf[0] != '\x1b' || buf[1] != '[')
+    {
+        return -1;
+    }
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
 void getWindowSize(int *rows, int *cols)
 {
     struct winsize ws;
-    if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
     {
-        return -1;
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+        {
+            return -1;
+        }
+        return getCursorPosition(rows, cols);
     }
     else
     {
