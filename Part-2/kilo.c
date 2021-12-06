a872f4ebeafd78bdbdb0b6a21af1682797134ef6
Mohammed Ibrahim <mohammedi@cpp.edu>
2021-12-06 15:22:06 -0800

    remap quit from q to Ctrl-Q

--- Part-2/kilo.c
+++ Part-2/kilo.c
@@ -6,6 +6,9 @@
 #include <stdio.h>
 #include <errno.h>
 
+/* defines */
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /* data */
 struct termios orig_termios;
 
@@ -69,7 +72,7 @@ int main()
             printf("%d ('%c')\r\n", c, c);
         }
 
-        if(c == 'q')
+        if(c == CTRL_KEY('q'))
         {
             break;
         }
