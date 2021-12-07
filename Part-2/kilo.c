67ce8459fdde779a73d0490f9974a4deee87a308
Mohammed Ibrahim <mohammedi@cpp.edu>
2021-12-06 18:24:50 -0800

    implement home and end keys

--- Part-2/kilo.c
+++ Part-2/kilo.c
@@ -19,6 +19,8 @@ enum editorKey
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    HOME_KEY,
+    END_KEY,
     PAGE_UP,
     PAGE_DOWN
 };
@@ -104,10 +106,18 @@ int editorReadKey()
                 {
                     switch(seq[1])
                     {
+                        case '1':
+                            return HOME_KEY;
+                        case '4':
+                            return END_KEY;
                         case '5':
                             return PAGE_UP;
                         case '6':
                             return PAGE_DOWN;
+                        case '7':
+                            return HOME_KEY;
+                        case '8':
+                            return END_KEY;
                     }
                 }
             }
@@ -123,7 +133,21 @@ int editorReadKey()
                         return ARROW_LEFT;
                     case 'D':
                         return ARROW_RIGHT;
+                    case 'H':
+                        return HOME_KEY;
+                    case 'F':
+                        return END_KEY;
                 }
+            }
+        }
+        else if(seq[0] == 'O')
+        {
+            switch (seq[1])
+            {
+                case 'H':
+                    return HOME_KEY;
+                case 'F':
+                    return END_KEY;
             }
         }
         return '\x1b';
@@ -320,6 +344,13 @@ void editorProcessKeypress()
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
+            break;
+
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+        case END_KEY:
+            E.cx = E.screencols - 1;
             break;
 
         case PAGE_UP: case PAGE_DOWN:
